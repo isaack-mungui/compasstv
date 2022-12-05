4216b67585f56b7050c5bc2e26a8a3a691b9f33c
Isaack Mungui <munguiisaack@gmail.com>
2022-12-05 12:35:45 +0300

    Added methods to creators controller

--- app/controllers/creators_controller.rb
+++ app/controllers/creators_controller.rb
@@ -1,2 +1,40 @@
 class CreatorsController < ApplicationController
+    def index
+        creators = Creator.all
+        render json: creators
+    end
+
+    def show
+        creator = Creator.find_by(id: params[:id])
+        if creator
+            render json: creator
+        else
+            render json: { error: "Creator not found" }, status: :not_found
+        end
+    end
+
+    def create
+        creator = Creator.create!(creator_params)
+        if creator
+            render json: creator
+        else
+            render json: { error: e.record.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    def update
+        creator = Creator.find_by(id: params[:id])
+        if creator
+            creator.update(creator_params)
+            render json: creator
+        else
+            render json: { error: "Creator not found" }, status: :not_found
+        end
+    end
+
+    private
+
+    def creator_params
+        params.permit(:name, :alias, :avatar, :bio, :facebook_handle, :instagram_handle, :twitter_handle)
+    end
 end
