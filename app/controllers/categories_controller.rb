1542a724d7a69ae5147bc7140aa7ff5650684575
Isaack Mungui <munguiisaack@gmail.com>
2022-12-05 12:34:00 +0300

    Added methods to controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,40 @@
 class CategoriesController < ApplicationController
+    def index
+        categories = Category.all
+        render json: categories
+    end
+
+    def show
+        category = Category.find_by(id: params[:id])
+        if category
+            render json: category, include: :content
+        else
+            render json: { error: "Category not found" }, status: :not_found
+        end
+    end
+
+    def create
+        category = Category.create!(category_params)
+        if category
+            render json: category
+        else
+            render json: { error: e.record.error.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    def update
+        category = Category.find_by(id: params[:id])
+        if category
+            category.update(category_params)
+            render json: category
+        else
+            render json: { error: "Category" }, status: :not_found
+        end
+    end
+
+    private
+
+    def category_params
+        params.permit(:name, :genre, :content_id)
+    end
 end
