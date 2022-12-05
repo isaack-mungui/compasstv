9ea4058d65b6775b43916ff12c32ec3767765a74
Isaack Mungui <munguiisaack@gmail.com>
2022-12-05 12:38:37 +0300

    Added controller actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :categories
-  resources :contents
-  resources :creators
+  resources :categories, only: [:index, :show, :create, :update]
+  resources :contents, only: [:index, :show, :create, :update, :destroy]
+  resources :creators, only: [:index, :show, :create, :update]
   resources :profiles, only: [:index, :show, :create, :update]
   resources :users, only: [:index, :show, :create, :update]
 end
