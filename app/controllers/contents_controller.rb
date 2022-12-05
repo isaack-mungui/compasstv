dbe3fc3f9e920d68a6359c4074b2ec83239d6700
Isaack Mungui <munguiisaack@gmail.com>
2022-12-05 12:34:50 +0300

    Added methods to controller

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,2 +1,50 @@
 class ContentsController < ApplicationController
+    def index
+        contents = Content.all
+        render json: contents
+    end
+
+    def show
+        content = Content.find_by(id: params[:id])
+        if content
+            render json: content, include: :creator
+        else
+            render json: { error: "Content not found" }, status: :not_found
+        end
+    end
+
+    def create
+        content = Content.create!(content_params)
+        if content
+            render json: content
+        else
+            render json: { error: e.record.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    def update
+        content = Content.find_by(id: params[:id])
+        if content
+            content.update(content_params)
+            render json: content
+        else
+            render json: { errors: "Content not found" }, status: :not_found
+        end
+    end
+
+    def destroy
+        content = Content.find_by(id: params[:id])
+        if content
+            content.destroy
+            head :no_content
+        else
+            render json: { error: "Content not found" }, status: :not_found
+        end
+    end
+
+    private
+
+    def content_params
+        params.permit(:title, :duration, :creator_id)
+    end
 end
