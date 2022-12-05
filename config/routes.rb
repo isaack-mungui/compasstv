260679304ea2ba930bc0f378f13061bb67f9d002
Isaack Mungui <munguiisaack@gmail.com>
2022-12-05 10:35:35 +0300

    Added routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :categories
+  resources :contents
+  resources :creators
+  resources :profiles
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
