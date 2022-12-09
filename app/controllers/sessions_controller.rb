6709d7c47b32adf8cbf8a724d39a06d051393437
Isaack Mungui <munguiisaack@gmail.com>
2022-12-09 22:48:03 +0300

    Renamed param

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,8 @@
 class SessionsController < ApplicationController
     # skip_before_action :authorize, only: :create
 
-    def click
-        byebug
-    end
-
     def create
-        user = User.find_by(email_address: params[:email_address])
+        user = User.find_by(email: params[:email])
         if user&.authenticate(params[:password])
             session[:user_id] = user.id
             render json: user, status: :created
