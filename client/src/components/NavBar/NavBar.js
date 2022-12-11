5cfbffcd85c7d5293ec96be26d42dfa9a087229b
Isaack Mungui <munguiisaack@gmail.com>
2022-12-11 18:05:58 +0300

    Refactored navbar

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -8,20 +8,6 @@ import './NavBar.css'
 function NavBar() {
     return (
         <div>
-            {/* <nav class="navbar bg-light">
-                <img src={logo} id="nav-logo" alt="Logo" />
-                <div class="container-fluid">
-                    <a class="navbar-brand home" href='/'>Home</a>
-                    <a class="navbar-brand videos" href='/videos'>Videos</a>
-                    <a class="navbar-brand podcasts" href='/podcasts'>Podcasts</a>
-                    <a class="navbar-brand live" href='/livestream' target="_blank">Live</a>
-                    <a class="navbar-brand sign-in" href='/signin'>Sign In</a>
-                    <button class="btn sign-up" type="submit">
-                        <a href='/signup' id='sign-up'>Sign Up</a>
-                    </button>
-                </div>
-            </nav> */}
-
             <header>
                 <img className="nav-logo" src={logo} alt="Logo" />
                 <nav>
