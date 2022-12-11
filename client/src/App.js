49cb8f1f3fc22afcd19c545547302f201e75ca2e
Isaack Mungui <munguiisaack@gmail.com>
2022-12-11 18:56:16 +0300

    Removed components

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import Home from './components/Home/Home'
 import NavBar from './components/NavBar/NavBar';
-import BrandSubscribe from './components/Subscribe/BrandSubscribe'
 import SignUp from './components/SignUp/SignUp';
 import SignIn from './components/SignIn/SignIn'
 
@@ -42,8 +41,6 @@ function App() {
         <Route path='/signup' element={<SignUp />} />
         <Route path='/signin' element={<SignIn onSignIn={handleSignIn}/>} />
       </Routes>
-      <BrandSubscribe />
-      <CreatorConnect />
       <Footer />
     </BrowserRouter>
   );
